609dc0f53c8226fda03278a59cc14c865a188234
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-05-16 19:51:12 -0500

    Adding more functions

--- src/main/java/services/UsersServices.java
+++ src/main/java/services/UsersServices.java
@@ -1,14 +1,13 @@
 package services;
 
+import model.Message;
 import model.Users;
 import providers.UsersProvider;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Path("usersA00369145")
 public class UsersServices {
@@ -21,15 +20,39 @@ public class UsersServices {
 
     @POST
     @Consumes("application/json")
+    @Produces("application/json")
     public Response createUser(Users user){
         UsersProvider provider = new UsersProvider();
         try {
             provider.create(user);
-            return Response.ok().build();
+            return Response.status(200).entity(user).build();
         } catch (SQLException e) {
             e.printStackTrace();
+            return Response.status(500).entity(new Message("Operación éxitosa. ")).build();
         }
-        return null;
     }
+
+    @GET
+    @Path("all")
+    public Response getAll(){
+        try {
+            UsersProvider provider = new UsersProvider();
+            ArrayList<Users> users = provider.getAllUsers();
+            return Response.ok(users)
+                    .header("Content-Type", "application/json")
+                    .build();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response
+                    .status(500)
+                    .entity(new Message("Failed"))
+                    .header("Content-Type","application/json")
+                    .build();
+        }
+
+    }
+
 
 }
